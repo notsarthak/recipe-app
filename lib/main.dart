e6305e7da321fcecd7acde782ec9b7693d6901c2
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2021-07-12 23:18:58 +0530

    Added padding to the grid and setup app theme

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,22 @@ class MyApp extends StatelessWidget {
       title: 'DeliMeals',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: "Raleway",
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontFamily: "RobotoCondensed",
+              ),
+            ),
       ),
       home: CategoriesScreen(),
     );
