06aa852b4e32fccfb78b0ef99db4282632b5c8f4
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2021-07-12 20:48:53 +0530

    Created widget for categories and rendered them on the categories screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
+import "./dummy_data.dart";
+import "./category_item.dart";
+
 class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridView(
-      children: <Widget>[],
+      children: DUMMY_CATEGORIES
+          .map(
+            (catData) => CategoryItem(
+              catData.title,
+              catData.color,
+            ),
+          )
+          .toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         crossAxisSpacing: 20,
