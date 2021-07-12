d1b4471305c28751732ff4f9c6c4a8d0bef5dd9e
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2021-07-12 22:29:17 +0530

    Made the categories screen as default screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -5,20 +5,25 @@ import "./category_item.dart";
 
 class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map(
-            (catData) => CategoryItem(
-              catData.title,
-              catData.color,
-            ),
-          )
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-        childAspectRatio: 3 / 2,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("DeliMeals"),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                catData.title,
+                catData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+          childAspectRatio: 3 / 2,
+        ),
       ),
     );
   }
