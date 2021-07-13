ec44a69e0c62afbf179a3ef491ee240e10169e20
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2021-07-13 18:51:38 +0530

    Passing data to the next screen when navigating to next screen with Navigator.push()

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,10 +1,15 @@
 import "package:flutter/material.dart";
 
 class CategoryMealsScreen extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Category Meals"),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text("These are the meals of this category"),
