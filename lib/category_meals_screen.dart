85d30ac69a92ae8a8e6ae2495b9010bb8adc2bb1
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2021-07-14 01:08:05 +0530

    Filtered meals of selected category from list of all meals and rendered simple text widgets for each meal

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 
+import "./dummy_data.dart";
+
 class CategoryMealsScreen extends StatelessWidget {
   static const String routeName = "/category-meals";
 
@@ -13,12 +15,19 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs["title"];
     final categoryId = routeArgs["id"];
-
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: ListView.builder(itemBuilder: (ctx, idx) {}, itemCount: ,),
+      body: ListView.builder(
+        itemBuilder: (ctx, idx) {
+          return Text(categoryMeals[idx].title);
+        },
+        itemCount: categoryMeals.length,
+      ),
     );
   }
 }
